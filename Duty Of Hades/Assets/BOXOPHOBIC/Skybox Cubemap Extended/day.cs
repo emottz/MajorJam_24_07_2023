aaf4687686b6d6f53a14c9ab0b38979ca23adefd
Emirhan Titiz <53917435+emottz@users.noreply.github.com>
2023-07-25 00:23:46 +0300

    oyun güncellendi
    24.07.23

---
+++ Duty Of Hades/Assets/BOXOPHOBIC/Skybox Cubemap Extended/day.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class day : MonoBehaviour
+{
+
+    public Material lighting;
+    public float value;
+
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
